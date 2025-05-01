b3a8071494bd32b6329878ded98dc4975dd0b85e
laboratorio <laboratorio@UPOAULA10710>
2025-05-01 07:12:11 -0500

    Agregar archivos de proyecto.

---
+++ HerreraR_ExamenPractticoVet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HerreraR_ExamenPractticoVet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
